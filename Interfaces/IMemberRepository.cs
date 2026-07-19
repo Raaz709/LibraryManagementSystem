1d640b9aca07ce6c138748a5a065047d87cbcb5b
Raaz709 <rajchapagain98@gmail.com>
2026-07-19 19:36:27 +0545

    Added CRUD operations in Repository

---
+++ Interfaces/IMemberRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManagementSystem.Model;
+
+namespace LibraryManagementSystem.Interfaces
+{
+    public interface IMemberRepository
+    {
+        Task<IEnumerable<Member>> GetMembers();
+
+        Task<Member?> GetMemberById(int id);
+
+        Task<int> AddMember(Member member);
+
+        Task<int> UpdateMember(Member member);
+
+        Task<int> DeleteMember(int id);
+    }
+}
---
+++ Repositories/MemberRepository.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Interfaces;
+using LibraryManagementSystem.Model;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class MemberRepository : IMemberRepository
+    {
+        private readonly DapperContext _context;
+
+        public MemberRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Member>> GetMembers()
+        {
+            var query = "SELECT * FROM Members";
+
+            using var connection = _context.CreateConnection();
+
+            return await connection.QueryAsync<Member>(query);
+
+        }
+
+        public async Task<Member?> GetMemberById(int id)
+        {
+            var query = "SELECT * FROM Members WHERE ID = @ID";
+
+            using var connection = _context.CreateConnection();
+
+            return await connection.QueryFirstOrDefaultAsync<Member>(query, new { ID = id });
+        }
+
+        public async Task<int> AddMember(Member member)
+        {
+            var query = @"INSERT INTO Members 
+                  (NAME, EMAIL, PHONE)
+                  VALUES
+                  (@NAME, @EMAIL, @PHONE)";
+
+            using var connection = _context.CreateConnection();
+
+            return await connection.ExecuteAsync(query, member);
+        }
+
+        public async Task<int> UpdateMember(Member member)
+        {
+            var query = @"UPDATE Members
+                  SET NAME = @NAME,
+                      EMAIL = @EMAIL,
+                      PHONE = @PHONE
+                  WHERE ID = @ID";
+
+            using var connection = _context.CreateConnection();
+
+            return await connection.ExecuteAsync(query, member);
+        }
+
+        public async Task<int> DeleteMember(int id)
+        {
+            var query = "DELETE FROM Members WHERE ID = @ID";
+
+            using var connection = _context.CreateConnection();
+
+            return await connection.ExecuteAsync(query, new { ID = id });
+        }
+    }
+}
